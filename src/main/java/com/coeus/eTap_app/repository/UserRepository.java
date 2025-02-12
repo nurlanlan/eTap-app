68eca26a876b9c3fedf280adf82df1ccf631b1f3
nurlanlan <nurahmad004@gmail.com>
2025-02-12 20:03:16 +0400

    user repository added

---
+++ src/main/java/com/coeus/eTap_app/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.coeus.eTap_app.repository;
+
+import com.coeus.eTap_app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByEmail(String email);
+}
