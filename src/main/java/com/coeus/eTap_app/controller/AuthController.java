5a5468ad06ef492fb1b034e9ab1d79e2f8028ac4
nurlanlan <nurahmad004@gmail.com>
2025-02-12 20:04:42 +0400

    auth controller

---
+++ src/main/java/com/coeus/eTap_app/controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.coeus.eTap_app.controller;
+
+import com.coeus.eTap_app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam String email, @RequestParam String password) {
+        try {
+            userService.register(email, password);
+            return "user registered successfully";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password) {
+        try {
+            userService.login(email, password);
+            return "User logged in successfully";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
