b3956365abbe86037d50dd82b4fabc6ac5dcbfa6
nurlanlan <nurahmad004@gmail.com>
2025-04-01 17:57:35 +0400

    vacancy repo init

---
+++ src/main/java/com/coeus/eTap_app/repository/VacancyRepository.java
@@ -0,0 +1,10 @@
+package com.coeus.eTap_app.repository;
+
+import com.coeus.eTap_app.model.Vacancy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VacancyRepository extends JpaRepository<Vacancy, Long> {
+     Vacancy findVacancyById(long id);
+}
