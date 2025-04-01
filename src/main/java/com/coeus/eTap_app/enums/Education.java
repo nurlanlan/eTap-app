5a00ca265ee8770300a6f18e4eb4f16f2098cf5f
nurlanlan <nurahmad004@gmail.com>
2025-04-01 17:59:20 +0400

    enum updated

--- src/main/java/com/coeus/eTap_app/enums/Education.java
+++ src/main/java/com/coeus/eTap_app/enums/Education.java
@@ -1,5 +1,9 @@
 package com.coeus.eTap_app.enums;
 
 public enum Education {
-    NO_EDUCATION, HIGH_SCHOOL, BACHELOR, MASTER, DOCTORATE,
+    NO_EDUCATION,
+    HIGH_SCHOOL,
+    BACHELOR,
+    MASTER,
+    DOCTORATE,
 }
