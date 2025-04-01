10f1d4864f16958697b09eab7e0f7fe20cec2dd4
nurlanlan <nurahmad004@gmail.com>
2025-04-01 17:58:06 +0400

    vacancy adding service released

--- src/main/java/com/coeus/eTap_app/service/VacancyService.java
+++ src/main/java/com/coeus/eTap_app/service/VacancyService.java
@@ -1,4 +1,43 @@
 package com.coeus.eTap_app.service;
 
+import com.coeus.eTap_app.enums.*;
+import com.coeus.eTap_app.model.Vacancy;
+import com.coeus.eTap_app.repository.VacancyRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VacancyService {
+    private final VacancyRepository vacancyRepository;
+
+    public VacancyService(VacancyRepository vacancyRepository) {
+        this.vacancyRepository = vacancyRepository;
+    }
+
+    public Vacancy addVacancy(String vacancyName,
+                              String vacancyDescription,
+                              Category category,
+                              City city,
+                              Education education,
+                              Experience experience,
+                              WorkSchedule workSchedule,
+                              EmploymentType employmentType,
+                              int salary
+    ) {
+        Vacancy vacancy = new Vacancy();
+        vacancy.setVacancyName(vacancyName);
+        vacancy.setVacancyDescription(vacancyDescription);
+        vacancy.setCategory(category);
+        vacancy.setCity(city);
+        vacancy.setEducation(education);
+        vacancy.setWorkSchedule(workSchedule);
+        vacancy.setEmploymentType(employmentType);
+        vacancy.setExperience(experience);
+        vacancy.setSalary(salary);
+        return vacancyRepository.save(vacancy);
+    }
+    public List<Vacancy> showAllVacancies() {
+        List<Vacancy> vacancies = vacancyRepository.findAll();
+        return vacancies;
+    }
 }
