8912d0f60f4e138ccc6d026d6d11b1571203cc99
nurlanlan <nurahmad004@gmail.com>
2025-04-02 17:07:56 +0400

    changed to optional

--- src/main/java/com/coeus/eTap_app/repository/CompanyRepository.java
+++ src/main/java/com/coeus/eTap_app/repository/CompanyRepository.java
@@ -4,7 +4,9 @@ import com.coeus.eTap_app.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-    Company findByCompanyEmail(String email);
+    Optional<Company> findByCompanyEmail(String email);
 }
